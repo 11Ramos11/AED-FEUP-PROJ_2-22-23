d844867edb582cbbb3cfc46ecc0e9f48949eb76d
Ramos <1joaoramos1@gmail.com>
2022-12-29 19:22:40 +0000

    Initial FlightMap done

---
+++ Graph/FlightMap.cpp
@@ -0,0 +1,53 @@
+// AED 2022/2023 - Aula Pratica 12
+// Pedro Ribeiro (DCC/FCUP) [last update: 11/12/2022]
+
+#include "FlightMap.h"
+
+// Constructor: nr nodes and direction (default: undirected)
+FlightMap::FlightMap(int airportNumber) :
+    airportNumber(airportNumber), airports(airportNumber) {}
+
+// Add edge from source to destination with a certain weight
+void FlightMap::addFlight(int source, int destination, Airline airline) {
+    if (source<1 || source>airportNumber || destination<1 || destination>airportNumber) return;
+
+    int distance = calculateDistance();
+
+    airports[source].flights.push_back({airline, destination, distance});
+}
+
+void calculateDistance(){
+
+}
+
+// Depth-First Search: example implementation
+void FlightMap::dfs(int v) {
+    // show node order
+    // cout << v << " ";
+    airports[v].visited = true;
+    for (auto e : airports[v].flights) {
+        int w = e.destination;
+        if (!airports[w].visited)
+            dfs(w);
+    }
+}
+
+// Breadth-First Search: example implementation
+void FlightMap::bfs(int v) {
+    for (int i=1; i<=airportNumber; i++) airports[i].visited = false;
+    queue<int> q; // queue of unvisited nodes
+    q.push(v);
+    airports[v].visited = true;
+    while (!q.empty()) { // while there are still unvisited nodes
+        int u = q.front(); q.pop();
+        // show node order
+        //cout << u << " ";
+        for (auto e : airports[u].flights) {
+            int w = e.destination;
+            if (!airports[w].visited) {
+                q.push(w);
+                airports[w].visited = true;
+            }
+        }
+    }
+}
---
+++ Graph/FlightMap.h
@@ -0,0 +1,61 @@
+// AED 2022/2023 - Aula Pratica 12
+// Pedro Ribeiro (DCC/FCUP) [last update: 11/12/2022]
+
+#ifndef _GRAPH_H_
+#define _GRAPH_H_
+
+#include <list>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+
+struct Airline{
+
+    string code;
+    string name;
+    string callSign;
+    string country;
+};
+
+class FlightMap {
+
+    struct Flight {
+
+        Airline airline;
+        int destination;   // Destination node
+        int distance; // An integer weight
+    };
+
+    struct Airport {
+        string code;
+        string name;
+        string city;
+        string country;
+        float latitude;
+        float longitude;
+
+        list<Flight> flights; // The list of outgoing edges (to adjacent nodes)
+        bool visited;   // As the node been visited on a search?
+    };
+
+    int airportNumber;  // Graph size (vertices are numbered from 1 to n)
+    vector<Airport> airports; // The list of nodes being represented
+
+public:
+    // Constructor: nr nodes and direction (default: undirected)
+    FlightMap(int airportNumber);
+
+    // Add edge from source to destination with a certain weight
+    void addFlight(int src, int dest, Airline airline);
+
+    // Depth-First Search: example implementation
+    void dfs(int v);
+
+    // Breadth-First Search: example implementation
+    void bfs(int v);
+};
+
+#endif
