e93c44e81ffe788fb957871fbc717a57bb9172a2
Matilde Simões <up202108782@up.pt>
2023-01-01 17:01:45 +0000

    initialize documentation

--- Database/Database.h
+++ Database/Database.h
@@ -4,24 +4,13 @@
  *  @bug No known bugs.
  */
 
-#ifndef AED_FEUP_PROJ_22_23_DATABASE_H
-#define AED_FEUP_PROJ_22_23_DATABASE_H
+#ifndef AED_FEUP_PROJ_2_22_23_DATABASE_H
+#define AED_FEUP_PROJ_2_22_23_DATABASE_H
 
-#include <set>
 #include <algorithm>
-#include <vector>
 #include <unordered_map>
 #include <unordered_set>
 #include "../Graph/FlightMap.h"
-
-/** @brief Database class to read all input files and store all information.
- *
- *  Intended to be instanced once and creates a database
- *  for use within the app.
- *  Holds prototype functions to read the following input files:
- *  "classes.csv", "classes_per_uc.csv" and "student_classes.csv".
- *  And stores their data accordingly.
- */
 
 struct City {
     string name;
@@ -38,32 +27,47 @@ struct City {
     };
 };
 
-
+/** @brief Database class to read all input files and store all information.
+ *
+ *  Intended to be instanced once and creates a database
+ *  for use within the app.
+ *  Holds prototype functions to read the following input files:
+ *  "airlines.csv", "airports.csv" and "flights.csv".
+ *  Stores their data accordingly.
+ */
 class Database {
 
 private:
 
+    //! @brief Holds all airports per city.
     unordered_map<City,  unordered_set<Airport, Airport::hashFunction>, City::hashFunction> airportsPerCity;
 
+    //! @brief Holds all airports..
     unordered_map<string,  Airport> airports;
 
+    //! @brief Holds all airlines.
     unordered_map<string,  Airline> airlines;
+
+    /** @brief Reads airlines input file and stores them accordingly.
+     *
+     *  Reads the file "airlines.csv" and for each line
+     *  it read a Airline code, name, its call sign and country.
+     *
+     *  @return Void.
+     */
+    void readAirlines();
 
     /** @brief Reads airports input file and stores them accordingly.
      *
-     *  Reads the file "classes_per_uc.csv" and for each line
-     *  it read a UC code and a class code.
-     *  If the UC code corresponds to a new UC, it creates
-     *  a new UC object and adds it to the vector of UCs.
-     *  With each class code read, a new class is added to the
-     *  vector of classes with the UC mentioned in the same line.
+     *  Reads the file "airports.csv" and for each line
+     *  it read a Airport code and name, a name of the city and country that belongs to
+     *  and its latitude and longitude.
+     *  If the Airport (name of the city, name of the country) corresponds to a new Airport,
+     *  it creates a new Airport object and adds it to the unordered_map of airports.
      *
      *  @return Void.
      */
     void readAirports();
-
-
-    void readAirlines();
 
 public:
 
