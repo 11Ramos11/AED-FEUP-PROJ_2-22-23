5e8f3f4c361de76089e450e1a6cfe6f4751e3af4
Alberto Serra <92031033+Alberto-Serra@users.noreply.github.com>
2023-01-03 17:06:29 +0000

    First menu structure added.

---
+++ menu/Menu.cpp
@@ -0,0 +1,110 @@
+//
+// Created by Alberto on 03/01/2023.
+//
+
+#include "Menu.h"
+#include <iostream>
+
+
+using namespace std;
+
+const string Menu::AIRPORT_LISTS = "Listings and related options to the Airports";
+const string Menu::MAX_FLIGHTS_LISTS = "Listings related to a maximum number of flights";
+const string Menu::GENERATE_TRAJECTORIES = "Generate flight trajectories based on origin and destination";
+
+const string Menu::QUIT_APPLICATION = "Quit the application";
+const string Menu::GO_BACK = "Go Back";
+const string Menu::QUIT_MESSAGE = "Thanks for using Air Transports application!";
+const string Menu::INVALID_INPUT = "Invalid input! Read options and try 0, 1, 2, 3 (...)";
+const string Menu::WRONG_INPUT = "That option does not exist! Read options and try 0, 1, 2, 3 (...)";
+
+
+const string Menu::BY_NUMBER = "Airport information by numbers";
+const string Menu::BY_LISTS = "Airport information by listings";
+
+const string Menu::FLIGHTS_NUMBER = "Number of flights of the airport";
+const string Menu::AIRLINES_NUMBER = "Number of airlines of the airport";
+const string Menu::DESTINATIONS_NUMBER = "Number of different flight destinations the airport";
+const string Menu::COUNTRIES_NUMBER = "Number different countries flights of the airport";
+
+const string Menu::AIRPORT_FLIGHTS = "List flights by airport";
+const string Menu::AIRPORT_AIRLINES = "List airlines by airport";
+const string Menu::AIRPORT_DESTINATIONS = "List flight destinations by airport";
+const string Menu::AIRPORT_COUNTRIES = "List flight countries by airport";
+
+Menu::Menu() {}
+
+void Menu::getWrongMessage() {
+    cout << "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n" << WRONG_INPUT
+         << "\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" << endl;
+}
+
+void Menu::getInvalidMessage() {
+    cout << "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n" << INVALID_INPUT
+         << "\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" << endl;
+}
+
+
+void Menu::startMenu() {
+    cout << "=============================================" << endl;
+    cout << "Welcome to the Air Transports application!" << endl;
+    cout << "=============================================" << endl;
+    cout << "Click ENTER for the options MENU";
+    cin.ignore();
+}
+
+void Menu::displayMenu() {
+
+    cout << "=============================================" << endl;
+    cout << "           Air Transports MENU" << endl;
+    cout << "=============================================" << endl;
+    cout << "1 - " << AIRPORT_LISTS << endl;
+    cout << "2 - " << MAX_FLIGHTS_LISTS << endl;
+    cout << "3 - " << GENERATE_TRAJECTORIES << endl;
+    cout << "9 - " << QUIT_APPLICATION << endl;
+    cout << "Insert option number > ";
+}
+
+void Menu::displaySearchInfoMenu() {
+
+    cout << endl;
+    cout << "=============================================" << endl;
+    cout << "           Airport Information Type Menu" << endl;
+    cout << "=============================================" << endl;
+    cout << "1 - " << BY_LISTS << endl;
+    cout << "2 - " << BY_NUMBER << endl;
+    cout << "9 - " << GO_BACK << endl;
+    cout << "Insert option number > ";
+}
+
+
+void Menu::breakLine() {
+    cout << "\n" << endl;
+}
+
+void Menu::displayNumbersMenu() {
+    cout << endl;
+    cout << "=============================================" << endl;
+    cout << " Check Airport information numbers" << endl;
+    cout << "=============================================" << endl;
+    cout << "1 - " << FLIGHTS_NUMBER << endl;
+    cout << "2 - " << AIRLINES_NUMBER << endl;
+    cout << "3 - " << DESTINATIONS_NUMBER << endl;
+    cout << "4 - " << COUNTRIES_NUMBER << endl;
+    cout << "9 - " << GO_BACK << endl;
+    cout << "Insert option number > ";
+}
+
+
+void Menu::displayListsMenu() {
+    cout << endl;
+    cout << "=============================================" << endl;
+    cout << "          Airport listing options" << endl;
+    cout << "=============================================" << endl;
+    cout << "1 - " << AIRPORT_FLIGHTS << endl;
+    cout << "2 - " << AIRPORT_AIRLINES << endl;
+    cout << "3 - " << AIRPORT_DESTINATIONS << endl;
+    cout << "4 - " << AIRPORT_COUNTRIES << endl;
+    cout << "9 - " << GO_BACK << endl;
+    cout << "Insert option number > ";
+}
---
+++ menu/Menu.h
@@ -0,0 +1,132 @@
+//
+// Created by Alberto on 03/01/2023.
+//
+
+#ifndef AED_FEUP_PROJ_2_22_23_MENU_H
+#define AED_FEUP_PROJ_2_22_23_MENU_H
+
+
+#include <string>
+
+using namespace std;
+
+/** @brief Holds all the displayable menus for the Application.
+ *
+ * Intended to be instanced once.
+ */
+class Menu {
+
+public:
+
+    //! @brief Defines the string to output in the menu, as an option, for the airport lists.
+    static const string AIRPORT_LISTS;
+
+    //! @brief Defines the string to output in the menu, as an option, for the max flights lists.
+    static const string MAX_FLIGHTS_LISTS;
+
+    //! @brief Defines the string to output in the menu, as an option to generate flight trajectories..
+    static const string GENERATE_TRAJECTORIES;
+
+    //! @brief Defines the string to output in the menu, as an option, to quit the application.
+    static const string QUIT_APPLICATION;
+
+    //! @brief Defines the string to output in the menu, as an option, to go back.
+    static const string GO_BACK;
+
+    //! @brief Defines the string to output in the menu, as a message, as a quit message.
+    static const string QUIT_MESSAGE;
+
+    //! @brief Defines the string to output in the menu, as a message, for invalid inputs.
+    static const string INVALID_INPUT;
+
+    //! @brief Defines the string to output in the menu, as a message, for wrong inputs.
+    static const string WRONG_INPUT;
+
+    //! @brief Defines the string to output in the menu, as a message, for wrong input usage.
+    static const string WRONG_USAGE_INPUT;
+
+    //! @brief Defines the string to output in the menu, as an option, to show the airport info numbers.
+    static const string BY_NUMBER;
+
+    //! @brief Defines the string to output in the menu, as an option, to show the airport info lists.
+    static const string BY_LISTS;
+
+    //! @brief Defines the string to output in the menu, as an option, to show the number of flights.
+    static const string FLIGHTS_NUMBER;
+
+    //! @brief Defines the string to output in the menu, as an option, to show the number of airlines in the airport.
+    static const string AIRLINES_NUMBER;
+
+    //! @brief Defines the string to output in the menu, as an option, to show the number of different destinations
+    static const string DESTINATIONS_NUMBER;
+
+    //! @brief Defines the string to output in the menu, as an option, to show the number of different countries.
+    static const string COUNTRIES_NUMBER;
+
+    //! @brief Defines the string to output in the menu, as an option, to list airport flights.
+    static const string AIRPORT_FLIGHTS;
+
+    //! @brief Defines the string to output in the menu, as an option, to list airport airlines.
+    static const string AIRPORT_AIRLINES;
+
+    //! @brief Defines the string to output in the menu, as an option, to list different airport flight destinations.
+    static const string AIRPORT_DESTINATIONS;
+
+    //! @brief Defines the string to output in the menu, as an option, to list different airport flight countries.
+    static const string AIRPORT_COUNTRIES;
+
+
+    //! @brief Does nothing.
+    Menu();
+
+    /** @brief Shows a message when user write a wrong input.
+     *
+     * @return Void.
+     */
+    void getWrongMessage();
+
+    /** @brief Shows a message when user write an invalid input.
+     *
+     * @return Void.
+     */
+    void getInvalidMessage();
+
+    /** @brief Displays the start menu.
+     *
+     * @return Void.
+     */
+    void startMenu();
+
+    /** @brief Displays the main menu.
+     *
+     * @return Void.
+     */
+    void displayMenu();
+
+    /** @brief Displays the menu with search type information.
+     *
+     * @return Void.
+     */
+    void displaySearchInfoMenu();
+
+    /** @brief Make a break line.
+     *
+     * @return Void.
+     */
+    void breakLine();
+
+    /** @brief Displays the menu with numbers information.
+     *
+     * @return Void.
+     */
+    void displayNumbersMenu();
+
+    /** @brief Displays the menu with listing information.
+     *
+     * @return Void.
+     */
+    void displayListsMenu();
+
+};
+
+#endif //AED_FEUP_PROJ_2_22_23_MENU_H
