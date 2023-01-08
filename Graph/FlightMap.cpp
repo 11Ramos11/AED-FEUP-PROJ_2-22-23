2fbe6ca7923484cf2138f288630984f8af613c45
Matilde Simões <up202108782@up.pt>
2023-01-08 02:54:09 +0000

    jghch

--- Graph/FlightMap.cpp
+++ Graph/FlightMap.cpp
@@ -5,7 +5,6 @@
  */
 
 #include "FlightMap.h"
-#include "Locals/Local.h"
 #include <algorithm>
 #include <stack>
 #include <string>
@@ -66,8 +65,10 @@ int FlightMap::minimumDistance(AirportPTR airportDepart, AirportPTR airportDesti
     return -1;
 }
 
-list<list<Flight>>
-FlightMap::getTrajectories(AirportPTR airportDepart, AirportPTR airportDestination, unordered_set<string> airlines, int minimumFlights) {
+list<list<Flight>>FlightMap::getTrajectories(AirportPTR airportDepart,
+                                             AirportPTR airportDestination,
+                                             unordered_set<string> airlines,
+                                             int minimumFlights) {
 
     list<list<Flight>> paths;
 
@@ -119,7 +120,9 @@ FlightMap::getTrajectories(AirportPTR airportDepart, AirportPTR airportDestinati
     return paths;
 }
 
-list<pair<AirportPTR, list<Flight>>> FlightMap::getFlights(LocalPTR origin, LocalPTR destination, unordered_set<string> airlines) {
+list<pair<AirportPTR, list<Flight>>> FlightMap::getFlights(LocalPTR origin,
+                                                           LocalPTR destination,
+                                                           unordered_set<string> airlines) {
 
     list<pair<AirportPTR, list<Flight>>> trajectoriesPairs;
     int minimum;
@@ -130,7 +133,8 @@ list<pair<AirportPTR, list<Flight>>> FlightMap::getFlights(LocalPTR origin, Loca
 
     for (const AirportPTR &originAirport: origin->getAirports(this))
         for (const AirportPTR &destAirport: destination->getAirports(this)) {
-            auto trajectories = getTrajectories(originAirport, destAirport, airlines, minimum);
+            auto trajectories = getTrajectories(originAirport,
+                                                destAirport, airlines, minimum);
             for (const auto &trajectory: trajectories)
                 trajectoriesPairs.push_back(make_pair(originAirport, trajectory));
         }
